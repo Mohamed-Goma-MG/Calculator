3a64dd94f7af35f8c8b02b37fd7b8129ee096128
Mohamed Goma <mohamed.goma.dev@gmail.com>
2026-09-21 16:12:19 +0300

    update: add vite & tsconfig configuration for aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,21 @@
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
-import { defineConfig } from 'vite'
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import { defineConfig } from "vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     port: Number(process.env.PORT) || 5173,
   },
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] }),
-  ],
-})
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  resolve: {
+    alias: {
+      "@/assets": path.resolve(__dirname, "./src/contexts"),
+      "@/components": path.resolve(__dirname, "./src/components"),
+      "@/contexts": path.resolve(__dirname, "./src/contexts"),
+      "@/reducers": path.resolve(__dirname, "./src/reducers"),
+      "@/scripts": path.resolve(__dirname, "./src/scripts"),
+    },
+  },
+});
